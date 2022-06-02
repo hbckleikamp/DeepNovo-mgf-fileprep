1fa89de4f6a237e5820d07769952947a951f89c8
hbckleikamp <49785660+hbckleikamp@users.noreply.github.com>
2022-06-02 13:17:17 +0200

    Update mzXML_idXML2mgf.py

--- mzXML_idXML2mgf.py
+++ mzXML_idXML2mgf.py
@@ -41,47 +41,63 @@ for file in mgf_files:
             # parse idXML
             idXML_file=file[:-3]+"idXML"
             if Path(idXML_file).name in os.listdir(folder):
-                pass
-            else: 
-                break
-            
-            with open (idXML_file,"r") as f:
-                xml_string=f.read()
 
-         
+            
+                with open (idXML_file,"r") as f:
+                    xml_string=f.read()
     
-    
-            #%% parse xml
-            
-            ps=[i.split("</PeptideHit>")[0] for i in xml_string.split('<PeptideIdentification')]
-            seqs=[i.split('sequence="')[1].split('"')[0] for i in ps] 
-        
-            scans=[i.split('scan=')[1].split('"')[0] if "scan=" in i else "" for i in ps ] 
-            df=pd.DataFrame(zip(seqs,scans),columns=["seqs","scans"])
-            df=df[df.scans!=""]
-            
-            df["seqs"]=df.seqs.apply(lambda x: re.sub("[\(\[].*?[\)\]]", "mod", x))
-            df.scans=df.scans.astype(str)
-            
+             
         
         
-        
-            #%% parse mgf
+                #%% parse xml
+                
+                ps=[i.split("</PeptideHit>")[0] for i in xml_string.split('<PeptideIdentification')]
+                seqs=[i.split('sequence="')[1].split('"')[0] for i in ps] 
             
-            lines=[l.splitlines()[0] for l in lines]
-            lines=np.array(lines)
-            lines=np.array(np.split(lines,np.argwhere(lines=="BEGIN IONS").flatten()[1:]))
+                scans=[i.split('scan=')[1].split('"')[0] if "scan=" in i else "" for i in ps ] 
+                df=pd.DataFrame(zip(seqs,scans),columns=["seqs","scans"])
+                df=df[df.scans!=""]
+                
+                df["seqs"]=df.seqs.apply(lambda x: re.sub("[\(\[].*?[\)\]]", "mod", x))
+                df.scans=df.scans.astype(str)
+                
             
-            scans=[]
             
-            ls=[]
-            for ix,l in enumerate(lines):
-                l=list(l)
             
-                s=l[3].split()
-                l[3]=s[0]
-                rt=l.pop(2)
-   
+                #%% parse mgf
+                
+                lines=[l.splitlines()[0] for l in lines]
+                lines=np.array(lines)
+                lines=np.array(np.split(lines,np.argwhere(lines=="BEGIN IONS").flatten()[1:]))
+                
+                scans=[]
+                
+                ls=[]
+                for ix,l in enumerate(lines):
+                    l=list(l)
+                
+                    s=l[3].split()
+                    l[3]=s[0]
+                    rt=l.pop(2)
+       
+                    
+                    if "scan" in l[1]:
+                        scan=str(l[1].split("scan=")[1].split(".")[0])
+                        l.insert(4,"SCANS="+scan)
+                        l.insert(5,rt)
+                        scans.append(scan)
+                        
+                        if scan in df.scans.values:
+                
+                            l.insert(6,"SEQ="+df[df.scans.values==scan].seqs.tolist()[0]) #"MAAAAAAACK") #mock peptide sequence
+                            l.append("")
+                            ls.extend(l)
+                    
+                ls="\n".join(ls)+"\n"
+                with open(file.replace(".mgf","_scan.mgf"), "w") as w:
+                    w.write(ls)
+                    
+
                 
                 if "scan" in l[1]:
                     scan=str(l[1].split("scan=")[1].split(".")[0])
